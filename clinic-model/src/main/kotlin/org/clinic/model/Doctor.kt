75828ce3d2f66b794bfae0b9f4f3c14614925090
szymon-puszcz <szymon.puszcz@new-gravity.com>
2021-03-13 18:52:20 +0100

    add cascading

--- clinic-model/src/main/kotlin/org/clinic/model/Doctor.kt
+++ clinic-model/src/main/kotlin/org/clinic/model/Doctor.kt
@@ -3,12 +3,14 @@ package org.clinic.model
 import org.clinic.model.enum.Specialisation
 import org.hibernate.annotations.GenericGenerator
 import java.util.UUID
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.EnumType
 import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
@@ -30,5 +32,8 @@ data class Doctor(
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    var specialisation: Specialisation
+    var specialisation: Specialisation,
+
+    @OneToMany(cascade = [CascadeType.ALL], mappedBy = "doctor")
+    var appointments: MutableList<Appointment> = mutableListOf()
 )
--- clinic-model/src/main/kotlin/org/clinic/model/Patient.kt
+++ clinic-model/src/main/kotlin/org/clinic/model/Patient.kt
@@ -2,10 +2,12 @@ package org.clinic.model
 
 import org.hibernate.annotations.GenericGenerator
 import java.util.UUID
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
@@ -27,4 +29,7 @@ data class Patient(
 
     @Column(nullable = false)
     var address: String,
+
+    @OneToMany(cascade = [CascadeType.ALL], mappedBy = "patient")
+    var appointments: MutableList<Appointment> = mutableListOf()
 )
